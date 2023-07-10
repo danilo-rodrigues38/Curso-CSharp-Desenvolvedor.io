d02a24ba9b090cd4dc0012706335c95c88dba535
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-07-09 21:48:52 -0300

    Create a new project

---
+++ TesteMVC5/TesteMVC5/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesteMVC5.Models;
+
+namespace TesteMVC5.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        [Route("novo-cliente")]
+        public ActionResult NovoCliente(Cliente cliente)
+        {
+            return View(cliente);
+        }
+    }
+}
---
+++ TesteMVC5/TesteMVC5/Models/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+
+namespace TesteMVC5.Models
+{
+    
+    public class Cliente
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [DisplayName("Nome")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório!!!")]
+        public string Name { get; set; }
+
+        [Required ( ErrorMessage = "O campo {0} é obrigatório!!!" )]
+        public string Cpf { get; set; }
+
+        [DisplayName ( "E-mail" )]
+        [Required ( ErrorMessage = "O campo {0} é obrigatório!!!" )]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido!")]
+        public string Email { get; set; }
+
+        [DisplayName ( "Data de nascimento" )]
+        [Required ( ErrorMessage = "O campo {0} é obrigatório!!!" )]
+        public DateTime Birtday { get; set; }
+
+        [DisplayName ( "Data do cadastro" )]
+        [Required ( ErrorMessage = "O campo {0} é obrigatório!!!" )]
+        public DateTime Cadastro { get; set; }
+    }
+}
